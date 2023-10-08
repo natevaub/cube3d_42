b96a0c0b9c3ad1175b47c85fda2e827617e0f71f
Your Name <nathan.vaubien@gmail.com>
2023-10-08 23:07:32 +0200

    Update

---
+++ includes/cube.h
@@ -0,0 +1,12 @@
+#ifndef CUBE_H
+# define CUBE_H
+
+#include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "../libs/ftprintf/ft_printf.h"
+#include "../libs/libft/libft.h"
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -4,10 +4,10 @@
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	// void	*mlx;
+	// void	*mlx_win;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	mlx_loop(mlx);
+	// mlx = mlx_init();
+	// mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	// mlx_loop(mlx);
 }
