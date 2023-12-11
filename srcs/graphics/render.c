3bbb893164203dd29ec9da511a2ff13a02b8e71d
Your Name <nathan.vaubien@gmail.com>
2023-12-11 12:50:35 +0100

    Update

--- srcs/graphics/render.c
+++ srcs/graphics/render.c
@@ -3,64 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nvaubien <nvaubien@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 06:43:35 by nvaubien          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/12/11 11:33:40 by nvaubien         ###   ########.fr       */
-=======
-/*   Updated: 2023/12/06 17:39:00 by rrouille         ###   ########.fr       */
->>>>>>> mouse_click
+/*   Updated: 2023/12/11 12:46:04 by nvaubien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../includes/cube.h"
 
-// void	draw_intersections(t_map *map, t_data *img, t_mapping *mapping)
-// {
-// 	int				i;
-// 	t_intersections	intersections;
-// 	t_vector		v;
-// 	// print v to see if it's correct
-
-// 	v = map->direction;
-// 	// printf("v: %f, %f\n", v.x, v.y);
-// 	intersections = compute_intersections(map->player_position, v, map);
-// 	i = 0;
-// 	while (i < intersections.size)
-// 	{
-// 		t_vector mapped = map_vec(intersections.points[i], map->mapping);
-// 		mapped.x += SCREEN_WIDTH / 2 - (map->columns * MAP_SCALE) / 2;
-// 		mapped.y += 10;
-// 		draw_disk(mapped.x, mapped.y, 3, img, RED);
-// 		// printf("intersection %d: %f, %f\n", i, intersections.points[i].x, intersections.points[i].y);
-// 		i++;
-// 	}
-// 	free(intersections.points);
-// 	// draw_line(img, (t_vector){.x = 100, .y = 100}, (t_vector){.x = 300, .y = 300}, RED);
-// }
-
 void	draw_intersections(t_map *map, t_data *img, t_mapping *mapping)
 {
 	int				i;
 	t_intersections	intersections;
 	t_vector		v;
-	// print v to see if it's correct
 
 	v = map->direction;
-	// printf("v: %f, %f\n", v.x, v.y);
 	intersections = compute_intersections(map->player_position, v, map);
 	i = 0;
 	while (i < intersections.size)
 	{
 		t_vector mapped = map_vec_adjust(intersections.points[i], map);
 		draw_disk(mapped.x, mapped.y, 3, img, RED);
-		// printf("intersection %d: %f, %f\n", i, intersections.points[i].x, intersections.points[i].y);
 		i++;
 	}
 	free(intersections.points);
-	// draw_line(img, (t_vector){.x = 100, .y = 100}, (t_vector){.x = 300, .y = 300}, RED);
 }
 
 void update_frame(t_map *map)
@@ -79,8 +47,6 @@ void	game_loop(t_map *map)
 	map->start_time = (int)time(NULL);
 	load_textures(map, &map->m_mlx);
 	event_manager(map);
-	// render_on_screen(m_mlx, map, mapping);
-	// render_on_screen(map);
 	mlx_loop_hook(map->m_mlx.mlx_ptr, game_loop_callback, map);
 	mlx_loop(map->m_mlx.mlx_ptr);
 	int current_time = (int)time(NULL);
@@ -98,7 +64,6 @@ int	game_loop_callback(t_map *map)
 	draw_view(map, &new_image, &map->mapping);
 	draw_minimap(map, &new_image, &map->mapping);
 	draw_player(map, &new_image, &map->mapping);
-	// draw_square_text(map, &new_image, &map->mapping);
 	
 
 	// Destroy the previous image
--- srcs/myminilibx/event_handler.c
+++ srcs/myminilibx/event_handler.c
@@ -6,7 +6,7 @@
 /*   By: nvaubien <nvaubien@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 00:23:48 by nvaubien          #+#    #+#             */
-/*   Updated: 2023/12/11 12:42:38 by nvaubien         ###   ########.fr       */
+/*   Updated: 2023/12/11 12:49:49 by nvaubien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	event_manager(t_map *map)
 {
-	int mouse_x;
-	int mouse_y;
+	int	mouse_x;
+	int	mouse_y;
 
 	mlx_hook(map->m_mlx.mlx_win, ON_KEYPRESS, 1L<<0, key_press, map);
 	mlx_mouse_hook(map->m_mlx.mlx_win, mouse_press, map);
@@ -25,8 +25,8 @@ void	event_manager(t_map *map)
 }
 
 int mouse_move(int x, int y, t_map *map) {
-	int dx;
-	float angle;
+	int		dx;
+	float	angle;
 
 	if (map->mouse_click) {
 		dx = x - map->mouse_x;
@@ -39,75 +39,34 @@ int mouse_move(int x, int y, t_map *map) {
 }
 
 int mouse_release(int button, int x, int y, t_map *map) {
-	if (button == MOUSE_LEFT) {
-		map->mouse_click = 0;
-		mlx_mouse_show();
-	}
-	return 0;
+	map->mouse_click = 0;
+	mlx_mouse_show();
+	return (0);
 }
 
 int mouse_press(int keycode, int x, int y, t_map *map) {
-
 	if (keycode == MOUSE_LEFT)
 	{
-		printf("Mouse left\n");
+		map->mouse_click = 1;
 	}
-	if (keycode == MOUSE_RIGHT)
+	else if (keycode == MOUSE_SCROLL_UP)
 	{
-		printf("Mouse right\n");
+		map->player_position = add(map->player_position, mul_scalar(map->direction, 0.1));
 	}
-	if (keycode == MOUSE_MIDDLE)
+	else if (keycode == MOUSE_SCROLL_DOWN)
 	{
-		printf("Mouse middle\n");
-	}
-	if (keycode == MOUSE_SCROLL_UP)
-	{
-		printf("Mouse scroll up\n");
-	}
-	if (keycode == MOUSE_SCROLL_DOWN)
-	{
-		printf("Mouse scroll down\n");
+		map->player_position = add(map->player_position, mul_scalar(map->direction, -0.1));
 	}
 	mlx_mouse_hide();
-	map->mouse_click = 1;
-	map->mouse_x = x;
-	map->mouse_y = y;
 	return (0);
-	// printf("Mouse position: %d, %d\n", x, y);
-	// map->mouse_x = x;
-	// map->mouse_y = y;
-	// map->mouse_click = handle_mouse(keycode, map, map->mouse_click);
-	// printf("Mouse click: %d\n", map->mouse_click);
-
-	// if (map->mouse_click) {
-	//     mlx_mouse_hide();
-	//     // Exemple de rotation - ajustez selon votre logique
-	//     map->direction = normalize(rotate(map->direction, M_PI / 4));
-	// } else {
-	//     mlx_mouse_show();
-	//     // Exemple de rotation - ajustez selon votre logique
-	//     map->direction = normalize(rotate(map->direction, -M_PI / 4));
-	// }
-
-	// return 0;
 }
+
 
 int	key_press(int keycode, t_map *map)
 {
-	// printf("Keycode: %d\n", keycode);
-	// printf("\nPlayer position: %f, %f\n", map->player_position.x, map->player_position.y);
 	handle_wasd(keycode, map);
 	handle_arrows(keycode, map);
 	handle_esc(keycode, map);
-	// if (keycode == LINUX_MOUSE_LEFT || keycode == MAC_MOUSE_LEFT)
-	// {
-	// 	int mouse_x;
-	// 	int mouse_y;
-
-	// 	mlx_mouse_get_pos(map->m_mlx.mlx_win, &mouse_x, &mouse_y);
-	// 	printf("Mouse position: %d, %d\n", mouse_x, mouse_y);
-	// }
-	// handle_mouse(keycode, map);
 	return (0);
 }
 
