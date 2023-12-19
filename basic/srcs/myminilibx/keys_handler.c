409aa813dfd24fe69967ffd74d85eef4469358d1
Raphael Rouiller <rrouille@c2r4s2.42lausanne.ch>
2023-12-19 15:34:42 +0100

    Corrected segfault

--- basic/srcs/myminilibx/keys_handler.c
+++ basic/srcs/myminilibx/keys_handler.c
@@ -6,7 +6,7 @@
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 12:13:03 by rrouille          #+#    #+#             */
-/*   Updated: 2023/12/19 11:04:27 by rrouille         ###   ########.fr       */
+/*   Updated: 2023/12/19 15:34:10 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,86 @@ void	handle_arrows(int keycode, t_map *map)
 		map->direction = normalize(rotate(map->direction, -2));
 }
 
+// void	handle_wasd(int keycode, t_map *map)
+// {
+// 	t_vector	perpendicular;
+// 	t_vector	dir;
+// 	t_vector	move;
+// 	t_vector	new_position;
+// 	float		epsilon;
+// 	int			is_moved;
+
+// 	perpendicular = normalize((t_vector){.x = -map->direction.y, .y = map->direction.x});
+// 	dir = normalize(map->direction);
+// 	is_moved = 0;
+// 	if (keycode == LINUX_W || keycode == MAC_W)
+// 	{
+// 		move =  mul_scalar(dir, 0.1);
+// 		is_moved = 1;
+// 	}
+// 	if (keycode == LINUX_A || keycode == MAC_A)
+// 	{
+// 		move = mul_scalar(perpendicular, -0.1);
+// 		is_moved = 1;
+// 	}
+// 	if (keycode == LINUX_S || keycode == MAC_S)
+// 	{
+// 		move = mul_scalar(dir, -0.1);
+// 		is_moved = 1;
+// 	}
+// 	if (keycode == LINUX_D || keycode == MAC_D)
+// 	{
+// 		move = mul_scalar(perpendicular, 0.1);
+// 		is_moved = 1;
+// 	}
+// 	if (is_moved)
+// 	{
+// 		epsilon = 0.01;
+// 		new_position = add(map->player_position, mul_scalar(move, 1+epsilon));
+// 		if (map->map[(int)new_position.y][(int)new_position.x] != '1')
+// 			map->player_position = new_position;
+// 	}
+// }
+
 void	handle_wasd(int keycode, t_map *map)
 {
+	t_vector	perpendicular;
+	t_vector	dir;
 	t_vector	move;
 	t_vector	new_position;
+	float		epsilon;
+	int			is_moved;
 
-	move = (t_vector){0, 0};
+	perpendicular = normalize((t_vector){.x = -map->direction.y, .y = map->direction.x});
+	dir = normalize(map->direction);
+	is_moved = 0;
 	if (keycode == LINUX_W || keycode == MAC_W)
-		move = mul_scalar(normalize(map->direction), map->speed);
-	else if (keycode == LINUX_S || keycode == MAC_S)
-		move = mul_scalar(normalize(map->direction), -map->speed);
-	else if (keycode == LINUX_D || keycode == MAC_D)
-		move = mul_scalar(normalize((t_vector){-map->direction.y,
-					map->direction.x}), map->speed);
-	else if (keycode == LINUX_A || keycode == MAC_A)
-		move = mul_scalar(normalize((t_vector){map->direction.y,
-					-map->direction.x}), map->speed);
-	new_position = add(map->player_position, move);
-	if (map->map[(int)new_position.y][(int)new_position.x] != '1'
-		&& map->map[(int)new_position.y][(int)new_position.x] != 'D')
-		map->player_position = new_position;
+	{
+		move =  mul_scalar(dir, 0.1);
+		is_moved = 1;
+	}
+	if (keycode == LINUX_A || keycode == MAC_A)
+	{
+		move = mul_scalar(perpendicular, -0.1);
+		is_moved = 1;
+	}
+	if (keycode == LINUX_S || keycode == MAC_S)
+	{
+		move = mul_scalar(dir, -0.1);
+		is_moved = 1;
+	}
+	if (keycode == LINUX_D || keycode == MAC_D)
+	{
+		move = mul_scalar(perpendicular, 0.1);
+		is_moved = 1;
+	}
+	if (is_moved)
+	{
+		epsilon = 0.01;
+		new_position = add(map->player_position, mul_scalar(move, 1+epsilon));
+		if (map->map[(int)new_position.y][(int)new_position.x] != '1')
+			map->player_position = new_position;
+	}
 }
 
 void	handle_esc(int keycode)
